a79310039a88c029d7486865faa1d14dd25cbb90
Sara Jimènez <sara.jimenez@hpc-submit01.scidom.de>
2024-09-30 15:25:52 +0200

    update

---
+++ notebooks/spatial/pertpy/jp_lab_pert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+#SBATCH -o slurm_jupyter_%j.txt
+#SBATCH -e slurm_error_%j.txt
+#SBATCH --job-name=jp_lab_pert
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sara.jimenez@helmholtz-munich.de
+#SBATCH --mem-per-cpu=20G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_p    
+#SBATCH --qos=gpu_normal
+#SBATCH --time=48:00:00
+
+source $HOME/.bash_profile
+ 
+conda deactivate
+
+conda activate pertpy 
+
+echo "Starting jupyter..."
+
+jupyter-lab --port 8873 --no-browser --ip=0.0.0.0 --notebook-dir=/lustre/groups/ml01/workspace/sara.jimenez/spatial_pancreas/notebooks/spatial/pertpy/
